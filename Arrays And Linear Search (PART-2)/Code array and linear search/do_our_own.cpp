6d3aa33de49c3ac5c591d03cec1bd9ef195c3b8d
MohdHozaifa <hozamohd@gmail.com>
2023-02-14 20:54:52 +0530

    Code

--- Arrays And Linear Search (PART-2)/Code array and linear search/do_our_own.cpp
+++ Arrays And Linear Search (PART-2)/Code array and linear search/do_our_own.cpp
@@ -2,3 +2,11 @@
 2) int arr[10] = {0}; -> All the values are initialized to 0
    int arr[10] = {1}; -> All values won't be initialized to 1. Find using memset how to initialize with 1 or any particular value other than 0.
 3) Implement swap function on your own way. (3 ways) --> Find all.
+
+
+1) Try union of two arrays with duplicate cases (Using intersection logic, mark common elements as INT_MIN and then use if condition). 
+2) Try to find 4 numbers in an array that give sum to a particular value on grounds of triplet and pair sum.
+3) Try to solve
+  i) left rotate array by one element
+  ii) majority element in array
+  iii) buy and sell stock - 1
